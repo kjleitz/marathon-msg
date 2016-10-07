14f894f4f3521489d01d874cc45608836930ff61
Keegan Leitz <kjleitz@MacBook-Pro.local>
2016-10-07 14:53:23 -0400

    tied users and timestamps to messages, made a historical chat log

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 // Not sure if best practice is to "use strict" globally or in functions
 "use strict";
 
-/* Easier to do initialize this way, but I don't like it...
+/*
+Easier to initialize this way, but I don't like it...
 
 var app = require("express")();
 var serv = require("http").Server(app);
 var io = require("socket.io")(serv);
 
 ...etc.
-
 */
 
 var express = require("express");
 var http = require("http");
 var socket = require("socket.io");
 var helmet = require("helmet");
-var auth = require("basic-auth");
 var fs = require("fs");
 var bodyParser = require("body-parser");
 var users = require(__dirname + "/users.json");
+// var auth = require("basic-auth");
 
 // create an express app
 var app = express();
@@ -41,10 +41,14 @@ var serv = http.Server(app);
 // mount a socket.io server onto our regular http server
 var io = socket(serv);
 
+// probably a bad idea, but I'm doing it because I'm a noob.
+// setting up a global "g" to put stuff onto for the session, at
+// least until a socket is established.
+var g = {};
+
 // statically serve the styles and scripts
 app.use("/styles", express.static(__dirname + "/public/styles"));
 app.use("/scripts", express.static(__dirname + "/public/scripts"));
-
 
 app.get("/marathon", function(req, res){
 	res.sendFile(__dirname + "/pages/auth.html");
@@ -56,6 +60,7 @@ app.post("/marathon", function(req, res){
 	if (!users[uname] || pwd !== users[uname]){
 		res.sendFile(__dirname + "/pages/reauth.html");
 	} else {
+		g.user = uname;
 		res.sendFile(__dirname + "/pages/main.html");
 	}
 });
@@ -64,24 +69,38 @@ app.post("/marathon", function(req, res){
 // then call a callback function and pass into it the SOCKET object that's
 // been instantiated client-side
 io.on("connection", function(socket){
-	console.log("a user connected");
+	socket.user = g.user;
 	socket.nickname = "Anonymous";
 
+	console.log(`User ${socket.user} connected`);
+
 	socket.on("disconnect", function(){
-		console.log('user disconnected');
+		console.log(`User ${socket.user} disconnected`);
 	});
 
 	socket.on("nickname set", function(nickname){
-		console.log("User \"" + socket.nickname + "\" is now \"" + nickname + "\"");
+		// console.log("User \"" + socket.nickname + "\" is now \"" + nickname + "\"");
+		console.log(`User ${socket.user} is now "${nickname}" (user was previously "${socket.nickname}")`);
 		socket.nickname = nickname;
 	});
 
 	socket.on("chat message", function(message){
-		console.log(socket.nickname + " says: " + message);
-		io.emit("message published", {
+		var msgObj = {
+			user: socket.user,
+			timestamp: Date.now(),
 			nickname: socket.nickname,
 			message: message
+		};
+
+		fs.appendFile("chat.log", "\n" + JSON.stringify(msgObj), function(err){
+			if(err){
+				throw err;
+			}
+			var time = new Date(msgObj.timestamp).toTimeString();
+			console.log(`${time} - ${socket.user} (${socket.nickname}): ${message}`);
 		});
+
+		io.emit("message published", msgObj);
 	});
 });
 
